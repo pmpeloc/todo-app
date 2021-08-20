5133a770da4ade8c2a552064dd4cd7584974ce99
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-08-20 03:17:47 -0300

    Clase 4 - 1 - Cambiar estado y eliminar tarea

--- js/index.js
+++ js/index.js
@@ -21,6 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
   mostrarTareas();
 });
 
+listaTarea.addEventListener('click', (e) => {
+  // Llamo a la función que observa donde hago click
+  btnAccion(e);
+});
+
 // console.log(Date.now());
 
 formulario.addEventListener('submit', (e) => {
@@ -61,14 +66,34 @@ const mostrarTareas = () => {
   listaTarea.innerHTML = '';
   // Recorremos el objeto de las tareas para mostarlo en pantalla
   Object.values(tareas).forEach((tarea) => {
-    console.log(tarea);
+    // console.log(tarea);
     // Primero clonamos el template
     const clon = template.cloneNode(true);
     // Al clon le asignamos el texto
     clon.querySelector('p').textContent = tarea.texto;
+    // Asignamos los ids a los íconos
+    clon.querySelectorAll('.fas')[0].dataset.id = tarea.id;
+    clon.querySelectorAll('.fas')[1].dataset.id = tarea.id;
     // Al fragment le asignamos el clon creado
     fragment.appendChild(clon);
   });
   // Renderizamos en pantalla las tareas
   listaTarea.appendChild(fragment);
+};
+
+const btnAccion = (e) => {
+  console.log(e.target.classList.contains('fa-check-circle'));
+  if (e.target.classList.contains('fa-check-circle')) {
+    console.log(e.target.dataset.id);
+    tareas[e.target.dataset.id].estado = true;
+    mostrarTareas();
+    console.log(tareas);
+  }
+  if (e.target.classList.contains('fa-minus-circle')) {
+    delete tareas[e.target.dataset.id];
+    mostrarTareas();
+    console.log(tareas);
+  }
+  // Evitar que otros eventos se activen fuera de este contenedor
+  e.stopPropagation();
 };
